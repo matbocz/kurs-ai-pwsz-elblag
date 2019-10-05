c8edb6cbe2997aea7ebb2cd6169e86292df8fcfe
Mateusz Boczarski <matbocz@outlook.com>
2019-10-05 18:40:14 +0200

    Create a0.php

---
+++ a0.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Lab 1 | Zad 0</title>
+        <meta name="author" content="Mateusz Boczarski 18560">
+    </head>
+
+    <body>
+    <!-- Utwórz skrypt PHP, który wypisze informacje o konfiguracji PHP na serwerze. -->
+
+    <h2>Lab 1 | Zad 0</h2>
+
+    <?php
+        phpinfo();
+    ?>
+    </body>
+</html>
