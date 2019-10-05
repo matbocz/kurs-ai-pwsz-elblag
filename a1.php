7efeaec53c1802c0346329f1bd340f7c61fdb417
Mateusz Boczarski <matbocz@outlook.com>
2019-10-05 18:48:26 +0200

    Create a1.php

---
+++ a1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Lab 1 | Zad 1</title>
+        <meta name="author" content="Mateusz Boczarski 18560">
+    </head>
+
+    <body>
+    <!-- 
+        Utwórz w PHP program, który wypisze Twoje imię i nazwisko. 
+        Wykonanie:
+        a) Zadeklaruj dwie zmienne: $imie i $nazwisko .
+        b) Wypisz na ekranie zdanie typu "Nazywam się Jan Kowalski."
+    -->
+
+    <h2>Lab 1 | Zad 1</h2>
+
+    <?php
+        $imie = 'Mateusz';
+        $nazwisko = 'Boczarski';
+
+        echo 'Nazywam się '.$imie.' '.$nazwisko.'.';
+    ?>
+    </body>
+</html>
