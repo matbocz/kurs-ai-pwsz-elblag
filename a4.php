9a2dd7ca141c2a43c1d01f7ca237a2ae4e285be9
Mateusz Boczarski <matbocz@outlook.com>
2019-10-05 20:11:41 +0200

    Create a4.php

---
+++ a4.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Lab 1 | Zad 4</title>
+        <meta name="author" content="Mateusz Boczarski 18560">
+    </head>
+
+    <body>
+    <!-- 
+        Napisać skrypt w PHP, który wypisze na ekranie informację o wartości zmiennej $a 
+        ($a>0 - liczba dodatnia, $a<0 - liczba ujemna, $a==0 - liczba równa zero).
+        Przykładowo: "Liczba 12.4 jest dodatnia."
+    -->
+
+    <h2>Lab 1 | Zad 4</h2>
+
+    <?php
+        $a = 2;
+
+        echo 'Zmienna: '.$a.'<br>';
+        
+        if($a > 0) 
+            echo 'Zmienna jest dodatnia';
+        elseif($a < 0) 
+            echo 'Zmienna jest ujemna';
+        else 
+            echo 'Zmienna jest równa zero';
+    ?>
+    </body>
+</html>
